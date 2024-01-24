f6db27efe677b512307b367f9bc3b773e65d099c
Piter-P <142358114+Piter-P@users.noreply.github.com>
2024-01-24 21:59:25 +0100

    Add files via upload

---
+++ tateti.py
@@ -0,0 +1,74 @@
+import random
+import time
+
+positions = []
+position = 1
+
+for i in range(0, 9):
+    positions.append(' ')
+
+
+def player_x(positions):
+    positions[int(input('donde: ')) - 1] = 'X'
+    return positions
+
+
+def computer_player(positions):
+    selection = random.randint(0, 8)
+    if positions[selection] == ' ':
+        positions[selection] = 'O'
+        return positions
+    else:
+        computer_player(positions)
+
+
+def empty_space(positions, position):
+    if positions[position - 1] == ' ':
+        return position
+    else:
+        return positions[position - 1]
+
+
+def print_game(position):
+    for i in range(0, 3):
+        for j in range(0, 3):
+            print(f' ', empty_space(positions, position), ' ', end=' ')
+            position += 1
+        print('')
+
+
+def win_game(positions):
+    # check vertical
+    for i in range(0, 3):
+        if positions[i] == positions[i + 3] == positions[i + 6]:
+            if positions[i] == 'X' or positions[i] == 'O':
+                win = positions[i]
+                return True, win
+    # check horizontal
+    for i in range(0, 9, 3):
+        if positions[i] == positions[i + 1] == positions[i + 2]:
+            if positions[i] == 'X' or positions[i] == 'O':
+                win = positions[i]
+                return True, win
+    # check diagonals
+    if positions[0] == positions[4] == positions[8]\
+    or positions[2] == positions[4] == positions[6]:
+        if positions[4] == 'X' or positions[4] == 'O':
+            win = positions[4]
+            return True, win
+    win = '0'
+    return False, win
+
+
+for i in range(0, 9):
+    if i % 2 == 0:
+        player_x(positions)
+    else:
+        print('computer choice: ')
+        time.sleep(1)
+        computer_player(positions)
+    print_game(position)
+    winner, win = win_game(positions)
+    if winner:
+        print(f'the winner is:', win)
+        break
